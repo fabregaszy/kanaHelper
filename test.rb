0e762884cb8b84eeae2d0444c32ee6bafc2c06c7
fabregaszy <fabregaszy@hotmail.com>
2013-04-15 22:00:49 +0800

    test.rb updated

--- test.rb
+++ test.rb
@@ -1,7 +1,15 @@
 # test translation from unicode to Kana
 
-for k in 12354..12400
-	print k.chr('UTF-8')
-	print (k+96).chr('UTF-8')
-	print "\n"
+for k in 12353..12400
+	next if k%2 == 0
+	
+	hiragana = k.chr('UTF-8')
+	katakana = (k+96).chr('UTF-8')
+	unknown_h  = (k+1).chr('UTF-8')
+	unknown_k  = (k+97).chr('UTF-8')
+	
+	print "#{hiragana} - #{katakana} - #{unknown_h} - #{unknown_k}\n"
 end
+
+
+gets	# to pause in Windows
